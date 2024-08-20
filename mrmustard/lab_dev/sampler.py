8370bee8bc8de4016fb6a44cddb281f832183e68
Anthony <anthonychytros@gmail.com>
2024-08-20 11:47:54 -0400

    cleaning up sampler

--- mrmustard/lab_dev/sampler.py
+++ mrmustard/lab_dev/sampler.py
@@ -13,51 +13,82 @@
 # limitations under the License.
 
 """
+Samplers for measurement devices.
 """
 
 from __future__ import annotations
 
-from typing import Any
+from typing import Sequence
 
 import numpy as np
 
 from mrmustard import math
 
 from .states import State, Number
+from .circuit_components import CircuitComponent
 from .circuit_components_utils import BtoQ
 
 __all__ = ["Sampler", "PNRSampler", "HomodyneSampler"]
 
 
 class Sampler:
-    r""" """
+    r"""
+    A sampler for measurements of quantum circuits.
+
+    Args:
+        meas_outcomes: The measurement outcomes for this sampler.
+        meas_ops: The optional measurement operators of this sampler.
+        probs: An optional probability distribution for this sampler.
+    """
 
     def __init__(
         self,
-        meas_out: list[Any],
-        meas_ops: list[State] | None = None,
-        probs: None | list[float] = None,
+        meas_outcomes: list[any],
+        meas_ops: CircuitComponent | list[CircuitComponent] | None = None,
+        probs: list[float] | None = None,
     ):
         self._meas_ops = meas_ops
-        self._meas_outcomes = meas_out
+        self._meas_outcomes = meas_outcomes
         self._probs = probs
 
     @property
-    def meas_ops(self):
-        r""" """
+    def meas_ops(self) -> CircuitComponent | list[CircuitComponent]:
+        r"""
+        The measurement operators of this sampler.
+        """
         return self._meas_ops
 
-    def sample(self, state: State, n_samples: int) -> list[Any]:
-        r""" """
+    @property
+    def meas_outcomes(self) -> list[any]:
+        r"""
+        The measurement outcomes of this sampler.
+        """
+        return self._meas_outcomes
+
+    def sample(self, state: State, n_samples: int) -> list[any]:
+        r"""
+        Returns a list of measurement samples on a specified state.
+
+        Args:
+            state: The state to generate samples of.
+            n_samples: The number of samples to generate.
+        """
         rng = np.random.default_rng()
         return [rng.choice(a=self._meas_outcomes, p=self.probs(state)) for _ in range(n_samples)]
 
-    def probs(self, state: State | None = None):
-        r""" """
+    def probabilities(self, state: State | None = None) -> list[float]:
+        r"""
+        Returns the probability distribution of this sampler. If ``probs`` was
+        not specified when initializing then a probability distribution is generated
+        using ``state``.
+
+        Args:
+            state: The state to generate the probability distribution with.
+        """
         if self._probs is None:
             states = [state >> meas_op.dual for meas_op in self.meas_ops]
             probs = [
-                state.probability if isinstance(state, State) else math.real(state)
+                state.probability if isinstance(state, State) else math.real(state) ** 2
                 for state in states
             ]
             return probs / sum(probs)
@@ -65,23 +96,40 @@ class Sampler:
 
 
 class PNRSampler(Sampler):
-    r""" """
+    r"""
+    A sampler for photon-number resolving (PNR) detectors.
 
-    def __init__(self, cutoff: int) -> None:
-        super().__init__(list(range(cutoff)), [Number([0], n).dm() for n in range(cutoff)])
+    Args:
+        modes: The measured modes.
+        cutoff: The photon number cutoff.
+    """
+
+    def __init__(self, modes: Sequence[int], cutoff: int) -> None:
+        super().__init__(list(range(cutoff)), [Number(modes, n).dm() for n in range(cutoff)])
 
 
 class HomodyneSampler(Sampler):
-    r""" """
+    r"""
+    A sampler for homodyne measurements.
 
-    def __init__(self, range: tuple[float, float] = (-5, 5), num: int = 100) -> None:
-        super().__init__(list(np.linspace(*range, num)))
+    Args:
+        modes: The measured modes.
+        xbounds: The range of ``x`` values.
+        num: The number of measurement outcomes.
+    """
 
-    def probs(self, state: State | None = None):
+    def __init__(
+        self, modes: Sequence[int], xbounds: tuple[float, float] = (-5, 5), num: int = 100
+    ) -> None:
+        super().__init__(list(np.linspace(*xbounds, num)), BtoQ(modes, phi=0))
+
+    def probabilities(self, state: State | None = None):
         if self._probs is None:
-            q_state = state >> BtoQ([0], phi=np.pi / 2)
-
-            probs = [q_state.representation(np.array([[q]]))[0] ** 2 for q in self._meas_outcomes]
-
+            q_state = state >> self.meas_ops
+            probs = [
+                math.real(q_state.representation(np.array([[q]]))[0]) ** 2
+                for q in self._meas_outcomes
+            ]
+            probs /= sum(probs)
             return probs
         return self._probs
